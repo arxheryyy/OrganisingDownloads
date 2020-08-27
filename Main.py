7c5f7450adcd153272d538c38eb675280ad980e3
Archer Chua <arxherchua@gmail.com>
2020-08-28 00:05:20 +0800

    Day 13 of 100daysofcode

--- Main.py
+++ Main.py
@@ -4,13 +4,20 @@ from os import walk
 from datetime import datetime
 
 DOWNLOADS = r'c:\Users\arxhe\Downloads'
-whatsapp = r'c:\Users\arxhe\Downloads\WhatsAppSetup.exe'
+file1 = r'c:\Users\arxhe\Downloads\2020-08'
 fileName = []
 paths = []
 ctimes = []
 dates = []
 dirs = []
 dirpaths = []
+datetimes = []
+monthstr = []
+yearstr = []
+daystr = []
+year = []
+day = []
+month = []
 x = 0
 for items in os.listdir(DOWNLOADS):
     if os.path.isfile(os.path.join(DOWNLOADS, items)):
@@ -18,8 +25,17 @@ for items in os.listdir(DOWNLOADS):
         path = (os.path.abspath(os.path.join(DOWNLOADS, items)))
         paths.append(path)
         ctimes.append(os.path.getctime(paths[x]))
-        dates.append(datetime.fromtimestamp(ctimes[x]).strftime('%Y-%m'))
+        yearstr.append(datetime.fromtimestamp(ctimes[x]).strftime('%y'))
+        year.append(int(yearstr[x]))
+        monthstr.append(datetime.fromtimestamp(ctimes[x]).strftime('%m'))
+        month.append(int(monthstr[x]))
+        daystr.append(datetime.fromtimestamp(ctimes[x]).strftime('%d'))
+        day.append(int(daystr[x]))
+        temp = datetime(year[x], month[x], day[x])
+        datetimes.append(temp)
+
         x = x+1
+
 a = 0
 for items in os.listdir(DOWNLOADS):
     if os.path.isdir(os.path.join(DOWNLOADS, items)):
@@ -29,11 +45,13 @@ for items in os.listdir(DOWNLOADS):
         a = a+1
 z = 0
 y = 0
-for date in dates:
-    if (dates[z] == dirs[y]):
-        shutil.move(paths[dates[z].index], dirpaths[dirs[y].index])
-        z = z+1
-        y = y+1
+aug = datetime(2020, 8, 12)
+
+for date in datetimes:
+    if (datetimes[z].month == aug.month):
+        shutil.move(paths[datetimes[z].index], file1)
 
     else:
         print("failed")
+z = z+1
+y = y+1
